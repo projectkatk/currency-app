7bc273d447c4d5bf6fd7751b44a6787998e9d05c
Kate Kim <kay.k.projects@gmail.com>
2024-04-04 00:32:47 +1000

    build fix

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import "../scss/Navigation.scss"
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
